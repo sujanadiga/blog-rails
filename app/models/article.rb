ddaa706cadf449315c7b9e87bcbb2150bd4e83e0
Sujan Adiga <sujan.a@practo.com>
2016-09-18 18:43:38 +0000

    Add model validation + styles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments
+
+  validates :title, presence: true
+  validates :text, presence: true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+
+  validates :comment, presence: true
 end
