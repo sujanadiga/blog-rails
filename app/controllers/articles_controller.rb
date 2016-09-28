c74fb9e5fe4084f32a410920ce15e2f01aca13f4
Sujan Adiga <sujan.a@practo.com>
2016-09-28 18:45:41 +0000

    Use partials in edit and new form and add article delete function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -32,6 +33,12 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
