23a6cd958dc019da8393c4bbf435c826f9ae274b
Sujan Adiga <sujan.a@practo.com>
2016-10-03 06:31:14 +0000

    Update action mailer smtp configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => Rails.application.secrets.mail_id,
+    :password             => Rails.application.secrets.mail_password,
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
