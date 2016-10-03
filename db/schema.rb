4d5a54b1e367d20160b133a6d6a4f8de1ed3541f
Sujan Adiga <sujan.a@practo.com>
2016-10-03 08:06:48 +0000

    Enable email confirmation - confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930091042) do
+ActiveRecord::Schema.define(version: 20161003075724) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -42,8 +42,13 @@ ActiveRecord::Schema.define(version: 20160930091042) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
